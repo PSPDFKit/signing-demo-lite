f54444573b5b57bbdc219d43f5c7e45fe699fecb
Siddharth Kamboj <71596674+Siddharth2001-July@users.noreply.github.com>
2025-07-31 17:42:21 +0530

    testing deployment config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  experimental: {
+    esmExternals: 'loose',
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
